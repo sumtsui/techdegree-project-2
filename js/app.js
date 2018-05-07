a2042404e22936642f9564975dd1a9f4bb4de39d
Sum Tsui <sumtsui@gmail.com>
2018-05-07 22:08:08 +0800

    added comments

--- js/app.js
+++ js/app.js
@@ -1,22 +1,29 @@
-const students = students53;
+// variable holding the student object array
+const students = students44;
+// the number of students shown per page
 const studentsPerPage = 10;
+// the total number of pages needed for pagination 
 const totalPage = getPageTotal(students);
 
+// prepare the page when loaded the first time. 
 showStudents(students, 1);
 showLinks();
 setActiveLink(1);
 
+// when user clicks one of the pagination links, gets the page number, calls showStudents(), and setActiveLink().
 document.querySelector('.pagination ul').addEventListener('click', (event) => {
 	let pageNum = event.target.text;
 	showStudents(students, pageNum);
 	setActiveLink(pageNum);
 });
 
+// render students to the current page.
 function showStudents(studentList, currentPage) {
 	let students = getStudentsOnPage(currentPage, studentList);
 	document.querySelector('.student-list').innerHTML = getStudentHTML(students);
 }
 
+// based on the totalPage number, render the pagination links to the page.
 function showLinks() {
 	let htmlString = '<ul>';
 	for (let i = 1; i <= totalPage; i++) {
@@ -27,20 +34,20 @@ function showLinks() {
 	document.querySelector('.pagination').innerHTML = htmlString;
 }
 
-// count from 1
-function getPageTotal(studentList) {
-	var total = Math.floor(studentList.length / 10);
-	return total += (studentList.length % 10) ? 1 : 0;
-}
-
+// takes the currentPage number and the whole student object array, calculate and return the sliced student object array.
 function getStudentsOnPage(currentPage, studentList) {
 	let endIndex = currentPage * studentsPerPage;
 	let startIndex = endIndex - studentsPerPage;
-	if (currentPage === totalPage) endIndex = studentList.length;
-	console.log('start: ', startIndex, 'end: ', endIndex);
 	return studentList.slice(startIndex, endIndex);
 }
 
+// based on the length of student object array and students per page, return the total pages needed. count from 1.
+function getPageTotal(studentList) {
+	var total = Math.floor(studentList.length / studentsPerPage);
+	return total += (studentList.length % studentsPerPage) ? 1 : 0;
+}
+
+// convert the given student object array to html.
 function getStudentHTML(studentList) {
 	var htmlString = '';
 	for (let i = 0; i < studentList.length; i++) {
@@ -55,6 +62,7 @@ function getStudentHTML(studentList) {
 	return htmlString;
 }
 
+// based on the current page number, give the "active" class to the correct link.
 function setActiveLink(currentPage) {
 	let links = document.querySelectorAll('.pagination a');
 	for (let i = 0; i < links.length; i++) {
